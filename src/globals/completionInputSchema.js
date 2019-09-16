a718274bf911c9d70a70a6473f3d117125839cbb
Eric De Sedas <eric.desedas.w@gmail.com>
2019-09-16 12:08:26 -0500

    Restored unique_name field to CompleteCondition form

--- src/globals/completionInputSchema.js
+++ src/globals/completionInputSchema.js
@@ -4,6 +4,12 @@ const completionInputSchema = {
     'npc_interaction': {
         name: 'NPC Interaction',
         parameters: {
+            unique_name: {
+                label: 'Unique Name',
+                type: 'text',
+                required: true,
+                placeholder: 'Must be unique inside the step'
+            },
             target_npc: {
                 label: 'Target NPC',
                 type: 'text',
@@ -23,6 +29,12 @@ const completionInputSchema = {
     'npc_finished_dialogues': {
         name: 'Finish NPC Dialogues',
         parameters: {
+            unique_name: {
+                label: 'Unique Name',
+                type: 'text',
+                required: true,
+                placeholder: 'Must be unique inside the step'
+            },
             target_npc: {
                 label: 'Target NPC',
                 type: 'text',
@@ -38,6 +50,12 @@ const completionInputSchema = {
     'enter_trigger_area': {
         name: 'Enter Trigger Area',
         parameters: {
+            unique_name: {
+                label: 'Unique Name',
+                type: 'text',
+                required: true,
+                placeholder: 'Must be unique inside the step'
+            },
             target_area_name: {
                 label: 'Target Area Name',
                 type: 'text'
@@ -48,6 +66,12 @@ const completionInputSchema = {
     'got_item': {
         name: 'Get Item',
         parameters: {
+            unique_name: {
+                label: 'Unique Name',
+                type: 'text',
+                required: true,
+                placeholder: 'Must be unique inside the step'
+            },
             required_item_id: {
                 label: 'Required item ID',
                 type: 'text'
@@ -58,6 +82,12 @@ const completionInputSchema = {
     'item_used': {
         name: 'Use Item',
         parameters: {
+            unique_name: {
+                label: 'Unique Name',
+                type: 'text',
+                required: true,
+                placeholder: 'Must be unique inside the step'
+            },
             used_item_id: {
                 label: 'Used item ID',
                 type: 'text'
@@ -68,6 +98,12 @@ const completionInputSchema = {
     'choice_selected': {
         name: 'Select Dialogue Choice',
         parameters: {
+            unique_name: {
+                label: 'Unique Name',
+                type: 'text',
+                required: true,
+                placeholder: 'Must be unique inside the step'
+            },
             choice: {
                 label: 'Choice',
                 type: 'text'
@@ -82,6 +118,12 @@ const completionInputSchema = {
     'advance_storyline': {
         name: 'Advance Storyline',
         parameters: {
+            unique_name: {
+                label: 'Unique Name',
+                type: 'text',
+                required: true,
+                placeholder: 'Must be unique inside the step'
+            },
             storyline: {
                 label: 'Storyline',
                 type: 'text'
@@ -96,6 +138,12 @@ const completionInputSchema = {
     'storyline_message': {
         name: 'Storyline Message',
         parameters: {
+            unique_name: {
+                label: 'Unique Name',
+                type: 'text',
+                required: true,
+                placeholder: 'Must be unique inside the step'
+            },
             message: {
                 label: 'Message',
                 type: 'text'
