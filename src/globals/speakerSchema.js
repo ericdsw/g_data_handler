6f2b25f4c81ffc3211f1724f9b8a5998fb94c19e
Eric De Sedas <eric.desedas.w@gmail.com>
2022-05-12 15:45:57 -0500

    Add the player character as a speaker target

--- src/globals/speakerSchema.js
+++ src/globals/speakerSchema.js
@@ -12,65 +12,83 @@ export const speakerImages = [
 ];
 
 const speakerSchema = {
+
   narrator: {
     image: null,
     name: null,
     voice_file: "default_voice.wav",
     target_object: "NONE",
   },
+
   phoenix: {
     image: "Phoenix_Serious.png",
     name: "Rocc",
     voice_file: "phoenix_voice.wav",
     target_object: "PhoenixNPC",
   },
+  
   vampire: {
     image: "Vega_mask_neutral.png",
     name: "Vega",
     voice_file: "vampire_voice.wav",
     target_object: "VegaNPC",
   },
+
   ghost: {
     image: "",
     name: "Cerie",
     voice_file: "ghost_voice.wav",
     target_object: "CerieNPC",
   },
+
   scientist: {
     image: "",
     name: "Ada",
     voice_file: "scientist_voice.wav",
     target_object: "AdaNPC",
   },
+
   default: {
     image: null,
     name: null,
     voice_file: "default_voice.wav",
   },
+
   phoenix_unknown: {
     image: null,
     name: "???",
     voice_file: "phoenix_voice.wav",
     target_object: "PhoenixNPC",
   },
+
   vampire_unknown: {
     image: null,
     name: "???",
     voice_file: "vampire_voice.wav",
     target_object: "VegaNPC",
   },
+
   ghost_unknown: {
     image: null,
     name: "???",
     voice_file: "ghost_voice.wav",
     target_object: "CerieNPC",
   },
+
   scientist_unknown: {
     image: null,
     name: "???",
     voice_file: "scientist_voice.wav",
     target_object: "AdaNPC",
   },
+
+  main_character: {
+    image: null,
+    name: "",
+    voice_file: "default_voice.wav",
+    target_object: "CONTROL_PLAYER"
+  }
+
 };
 
 export default speakerSchema;
