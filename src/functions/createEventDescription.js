82abc101d010eb09ef8157513ce574292727ef7d
Eric De Sedas <eric.desedas.w@gmail.com>
2022-05-28 22:11:28 -0500

    Add missing event descriptions

--- src/functions/createEventDescription.js
+++ src/functions/createEventDescription.js
@@ -145,6 +145,13 @@ export default function createEventDescription(type, parameters) {
       }`;
     case 'give_currency':
       return `Will add ${parameters.given_amount} to the wallet`;
+    case 'seed_next_run':
+      return `Will apply the seed ${parameters.seed_name} to the next run.`
+    case 'toggle_player_control':
+      if (parameters.control_status) {
+        return "Will return control to the player";
+      }
+      return "Will take away control from the player";
     default:
       return '';
   }
